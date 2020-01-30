2c069699b8c122ea015924fb6464fa112e5122f9
Victor Souza <victorsouza@programmer.net>
2020-01-29 21:59:23 -0300

    Criação metado initialize - Colocando data e hora no display da calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,63 @@
 class CalcController {
 
+    constructor(){
+
+        // _ = private
+        
+        this._locale = "pt-br";
+        this._displayEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._hourEl = document.querySelector('#hora');
+        this._displayCalc;
+        this._currentDate;
+        this.initialize();
+
+    };
+
+    initialize(){ 
+
+        this.displayDateHour();
+
+        setInterval(() => {
+
+            this.displayDateHour();
+
+        }, 1000);
+    }
+
+    displayDateHour(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayHour = this.currentDate.toLocaleTimeString(this._locale); 
+    }
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayHour(){
+        return this._hourEl.innerHTML;
+    }
+
+    set displayHour(value){
+        return this._hourEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
 };
