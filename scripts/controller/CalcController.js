a84361367151c16a22a0c4e96ca05ec3d750d9e0
Victor Souza <victorsouza@programmer.net>
2020-01-29 22:52:15 -0300

    Criação metodo initButtons e EventListener

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -11,6 +11,7 @@ class CalcController {
         this._displayCalc;
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     };
 
@@ -23,6 +24,28 @@ class CalcController {
             this.displayDateHour();
 
         }, 1000);
+    }
+
+    addEventListenerAll(elements, events, fn){
+
+        events.split(' ').forEach(event => {
+            elements.addEventListener(event, fn, false);
+       });
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+            this.addEventListenerAll(btn, 'click drag', (e) => {
+                console.log(btn);
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
+                btn.style.cursor = "pointer";
+            });
+        });
     }
 
     displayDateHour(){
